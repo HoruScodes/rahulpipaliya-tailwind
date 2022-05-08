f2ca0ef07d689d3e3da07f5958b42f1057829aec
horuscodes <pipaliya1997@icloud.com>
2022-05-08 17:37:28 -0400

    add animated cursor

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,37 @@
 import "tailwindcss/tailwind.css";
 import "../styles/global.css";
+
 import Head from "next/head";
 import { ThemeProvider } from "next-themes";
+import { useRouter } from "next/router";
+import React, { useRef, useEffect } from "react";
+
+import Cursor from "./Cursor";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const cursorRef = useRef(null);
+
+  useEffect(() => {
+    router.events.on("routeChangeComplete", progressDone);
+    router.events.on("routeChangeError", progressDone);
+
+    return () => {
+      router.events.off("routeChangeComplete", progressDone);
+      router.events.off("routeChangeError", progressDone);
+    };
+  }, []);
+
+  const progressDone = () => {
+    const { cursor } = cursorRef;
+    if (cursor) {
+      cursor.update();
+    }
+  };
+
   return (
     <>
+      <Cursor ref={cursorRef} />
       <Head>
         <title>Rahul Pipaliya</title>
       </Head>
